bfea652d0b91d576fa4fcd398a0bceea6e5ae5f4
AnonymousFool <qsw66793@uga.edu>
2019-02-22 16:22:34 -0500

    Updated movingMNISTExplorer.py Now allows user to choose index and filename for video. Raises exception if invalid index or filename is provided.

--- models/movingMNISTExplorer.py
+++ models/movingMNISTExplorer.py
@@ -1,15 +1,32 @@
 import numpy as np
 import imageio
+import argparse
 
 """
-Loads the first video data from the moving mnist dataset
-Saves the data as an mp4 named 'number.mp4'
+Loads video data from the observation in the moving MNIST dataset corresponding to given index
+Saves the data as an mp4 named by the provided filename
 
 Uses imageio. Requires installation of ffmpeg.
 
 @author Quinn Wyner
 """
-mnist = np.load("../data/mnist_test_seq.npy")
-test = mnist[:,0]
 
-imageio.mimwrite('number.mp4', test, fps=10)
+#Parses user arguments
+parser = argparse.ArgumentParser(description='Moving MNIST Explorer')
+parser.add_argument('--index', type=int, default=-1, metavar='i',
+                    help = 'index from 0 to 9999 in moving mnist dataset from which to generate a video')
+parser.add_argument('--filename', type=str, default='', metavar='F',
+                    help = 'name of file to which the video should be saved')
+args = parser.parse_args()
+
+#Loads moving MNIST dataset
+mnist = np.load("../data/mnist_test_seq.npy")
+
+#Saves video as mp4 or raises Exception if invalid index and filename are provided
+if(args.index >= 0 and args.index < 10000):
+    if(args.filename != ''):
+        imageio.mimwrite(args.filename, mnist[:,args.index], fps=10)
+    else:
+        raise Exception('filename must be defined')
+elif(args.filename != ''):
+    raise Exception('valid index between 0 and 9999 must be defined')
