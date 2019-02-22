c623fabf50f8c9bec1e001d724aa21d053e6a13f
AnonymousFool <qsw66793@uga.edu>
2019-02-22 15:51:45 -0500

    Updated movingMNISTExplorer.py Now uses imageio to save the video as an mp4. Most importantly, now works.

--- models/movingMNISTExplorer.py
+++ models/movingMNISTExplorer.py
@@ -1,25 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as ani
+import imageio
+
+"""
+Loads the first video data from the moving mnist dataset
+Saves the data as an mp4 named 'number.mp4'
+
+Uses imageio. Requires installation of ffmpeg.
+
+@author Quinn Wyner
+"""
 mnist = np.load("../data/mnist_test_seq.npy")
-print(mnist.shape)
-
 test = mnist[:,0]
-print(test.shape)
 
-fig = plt.figure()
-im = plt.imshow(test[0], interpolation='none')
-ax = plt.axes(xlim=(0,64), ylim=(0,64))
-line, = ax.plot([], [], lw=2)
-
-def init():
-    im.set_data(test[0])
-    return [im]
-    
-def animate(i):
-    im.set_array(test[i])
-    return [im]
-    
-anim = ani.FuncAnimation(fig, animate, init_func=init, frames=20, interval=20, blit=True)
-
-plt.show()
+imageio.mimwrite('number.mp4', test, fps=10)
