ee06e6a091dd47ceaf35b26f858f0c275a70c748
AnonymousFool <qsw66793@uga.edu>
2019-05-25 16:25:49 -0400

    Updated mMNISTflat.py Added a main guard to allow use in external models

--- models/mMNISTflat.py
+++ models/mMNISTflat.py
@@ -13,58 +13,6 @@ Splits the dataset into training and testing sets and constructs loaders for the
 
 @author Quinn Wyner, Davis Jackson
 """
-
-#Parses user arguments
-parser = argparse.ArgumentParser(description='Moving MNIST Explorer')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--testSplit', type=float, default=.2, metavar='%',
-                    help='portion of dataset to test on (default: .2)')
-parser.add_argument('--source', type=str, default='../data/mnist_test_seq.npy', metavar='S',
-                    help = 'path to moving MNIST dataset (default: \'../data/mnist_test_seq.npy\')')
-args = parser.parse_args()
-
-def genDataset(source='../data/mnist_test_seq.npy'):
-    #Loads moving MNIST dataset
-    mnist = np.load(source)
-
-    #movingMNISTDataset class    
-    class movingMNISTDataset(Dataset):
-        """
-        Initializes dataset
-        @param npArray (numpy.array): moving MNIST dataset
-        @param transform(callable, optional): Optional transform to be applied on a sample.
-        """
-        def __init__(self, npArray, transform=None):
-            self.npArray = npArray
-            self.frameCount = (self.npArray.shape)[0]
-            self.vidCount = (self.npArray.shape)[1]
-            self.transform = transform
-           
-        """
-        Gets number of observations in dataset
-        @return number of observations
-        """
-        def __len__(self):
-            return self.frameCount * self.vidCount
-        
-        """
-        Gets the observation at a given index
-        @param index (int): index corresponding to observation which is to be returned
-        @return Tensor observation corresponding to given index
-        """
-        def __getitem__(self, index):
-            obs = self.npArray[index % self.frameCount, index // self.frameCount,:,:,np.newaxis]
-            if self.transform:
-                obs = self.transform(obs)
-            return obs
-    
-    #Constructs Pytorch Dataset from moving MNIST data
-    return movingMNISTDataset(npArray=mnist, transform=transforms.ToTensor())
     
 def genLoaders(data, batch_size=128, no_cuda=False, seed=1, testSplit=.2):
     cuda = not no_cuda and torch.cuda.is_available()
@@ -85,5 +33,57 @@ def genLoaders(data, batch_size=128, no_cuda=False, seed=1, testSplit=.2):
     
     return train_loader, test_loader
 
-data = genDataset(args.source)
-genLoaders(data, args.batch_size, args.no_cuda, args.seed, args.testSplit)
+if __name__ == "__main__":
+    #Parses user arguments
+    parser = argparse.ArgumentParser(description='mMNISTflat')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--testSplit', type=float, default=.2, metavar='%',
+                        help='portion of dataset to test on (default: .2)')
+    parser.add_argument('--source', type=str, default='../data/mnist_test_seq.npy', metavar='S',
+                        help = 'path to moving MNIST dataset (default: \'../data/mnist_test_seq.npy\')')
+    args = parser.parse_args()
+
+    def genDataset(source='../data/mnist_test_seq.npy'):
+        #Loads moving MNIST dataset
+        mnist = np.load(source)
+
+        #movingMNISTDataset class    
+        class movingMNISTDataset(Dataset):
+            """
+            Initializes dataset
+            @param npArray (numpy.array): moving MNIST dataset
+            @param transform(callable, optional): Optional transform to be applied on a sample.
+            """
+            def __init__(self, npArray, transform=None):
+                self.npArray = npArray
+                self.frameCount = (self.npArray.shape)[0]
+                self.vidCount = (self.npArray.shape)[1]
+                self.transform = transform
+               
+            """
+            Gets number of observations in dataset
+            @return number of observations
+            """
+            def __len__(self):
+                return self.frameCount * self.vidCount
+            
+            """
+            Gets the observation at a given index
+            @param index (int): index corresponding to observation which is to be returned
+            @return Tensor observation corresponding to given index
+            """
+            def __getitem__(self, index):
+                obs = self.npArray[index % self.frameCount, index // self.frameCount,:,:,np.newaxis]
+                if self.transform:
+                    obs = self.transform(obs)
+                return obs
+        
+        #Constructs Pytorch Dataset from moving MNIST data
+        return movingMNISTDataset(npArray=mnist, transform=transforms.ToTensor())
+    data = genDataset(args.source)
+    genLoaders(data, args.batch_size, args.no_cuda, args.seed, args.testSplit)
