65dd03ec9b0b42b9e175e45cc7aadf6698db0846
micky774 <zainmeekail@gmail.com>
2019-06-05 00:19:20 +0000

    Prepared experiment 2

--- models/task_queue.sh
+++ models/task_queue.sh
@@ -2,5 +2,5 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python VtPVAE.py --epochs 40 --lsdim 12 --batch-size 1000 --save weights/exp1/bs1000.h5; python VtPVAE.py --epochs 40 --lsdim 12 --save weights/exp1/bs128.h5
+python VtPVAE.py --epochs 20 --lsdim 12 --batch-size 1000 --gamma .05 --save weights/exp2/g05.h5; python VtPVAE.py --epochs 20 --lsdim 12 --gamma .1 --save weights/exp2/g10.h5; python VtPVAE.py --epochs 20 --lsdim 12 --gamma .15 --save weights/exp2/g15.h5
 
