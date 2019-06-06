3899c150bfda483e076f3ed5f5fc17532e79e6a2
micky774 <zainmeekail@gmail.com>
2019-06-06 03:30:32 +0000

    Ran experiment 3

--- models/task_queue.sh
+++ models/task_queue.sh
@@ -2,5 +2,11 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python VtPVAE.py --epochs 20 --lsdim 12 --gamma .05 --save weights/exp2/g05b128.h5
-
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p5.h5 --pseudos 5; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p15.h5 --pseudos 15; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p25.h5 --pseudos 25; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p35.h5 --pseudos 35; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p45.h5 --pseudos 45; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p55.h5 --pseudos 55; 
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p65.h5 --pseudos 65;
+python VtPVAE.py  --lsdim 12 --gamma .15 --save weights/exp3/p75.h5 --pseudos 75;
---
+++ models/test_queue.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+python VtPVAE.py --epochs 1 --batch-size 1000; 
+python VtPVAE.py --epochs 1 --batch-size 1000; 
