b75164dca0cdfcb0e6dac4b4ad589e4dd534e9ff
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-13 02:20:34 +0000

    First phase of 3dconv batch-size experiment

--- models/task_queue2.sh
+++ models/task_queue2.sh
@@ -2,6 +2,16 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python 3DConv.py  --epochs 30 --lsdim 16 --save weights/3dconv/b1.h5 --pseudos 25 --batch-size 64;
-python 3DConv.py  --epochs 30 --lsdim 16 --save weights/3dconv/b2.h5 --pseudos 25 --batch-size 128;
-python 3DConv.py  --epochs 30 --lsdim 16 --save weights/3dconv/b3.h5 --pseudos 25 --batch-size 256;
+python 3DConv.py --epochs 30 --lsdim 16 --load weights/3dconv/b1.h5 --pseudos 25 --batch-size 64 --save weights/3dconv/b1e2.h5;
+
+python 3DConv.py --epochs 30 --lsdim 16 --load weights/3dconv/b2.h5 --pseudos 25 --batch-size 128 --save weights/3dconv/b2e2.h5
+
+python 3DConv.py --epochs 30 --lsdim 16 --save weights/3dconv/b3.h5 --pseudos 25 --batch-size 200;
+
+python 3DConv.py --epochs 30 --lsdim 16 --load weights/3dconv/b3.h5 --pseudos 25 --batch-size 200 --save weights/3dconv/b3e2.h5;
+
+python 3Dconv.py --epochs 30 --lsdim 16 --load weights/3dconv/b1e2.h5 --pseudos 25 --batch-size 64 --save weights/3dconv/b1e3.h5
+
+python 3Dconv.py --epochs 30 --lsdim 16 --load weights/3dconv/b2e2.h5 --pseudos 25 --batch-size 128 --save weights/3dconv/b2e3.h5
+
+python 3Dconv.py --epochs 30 --lsdim 16 --load weights/3dconv/b3e2.h5 --pseudos 25 --batch-size 200 --save weights/3dconv/b3e3.h5
