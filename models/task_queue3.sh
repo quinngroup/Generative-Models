72734141d55820638a2b8f2e0f7b89796b11846e
micky774 <zainmeekail@gmail.com>
2019-06-13 02:23:13 +0000

    First part of exp6

--- models/task_queue3.sh
+++ models/task_queue3.sh
@@ -2,5 +2,4 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python VtPVAE.py  --epochs 30 --lsdim 12 --save weights/exp6/w1.h5 --pseudos 25;
-python vvtp.py  --epochs 30 --lsdim 12 --save weights/exp6/w2.h5 --pseudos 25;
+python vvtp.py  --epochs 20 --lsdim 12 --save weights/exp6/w2.h5 --pseudos 25;
