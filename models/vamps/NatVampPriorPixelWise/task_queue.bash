d75475cbeacdfaf311b0df78ca4efaa69fdbfc04
Djaxkappa <djaxkappa@gmail.com>
2019-06-18 19:38:17 -0400

    Fixed Experiment10

--- models/vamps/NatVampPriorPixelWise/task_queue.bash
+++ models/vamps/NatVampPriorPixelWise/task_queue.bash
@@ -3,8 +3,8 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 python NatVampPrior --save ../../weights/exp5/w; 
-python NatVampPriorPixelwise_1 --save ../../weights/exp5/w1; 
-python NatVampPriorPixelwise_2 --save ../../weights/exp5/w2; 
-python NatVampPriorPixelwise_First --save ../../weights/exp5/wF; 
-python NatVampPriorPixelwise_FirstLast --save ../../weights/exp5/wFL; 
-python NatVampPriorPixelwise_Last --save ../../weights/exp5/wL; 
+python NatVampPriorPixelwise_1 --save ../../weights/exp10/w1; 
+python NatVampPriorPixelwise_2 --save ../../weights/exp10/w2; 
+python NatVampPriorPixelwise_First --save ../../weights/exp10/wF; 
+python NatVampPriorPixelwise_FirstLast --save ../../weights/exp10/wFL; 
+python NatVampPriorPixelwise_Last --save ../../weights/exp10/wL; 
