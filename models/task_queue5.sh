b42e3a2bdaf70b086825f4b37bcd6e8cdfb110a3
micky774 <zainmeekail@gmail.com>
2019-06-20 15:14:55 -0400

    Moved weights, and updated exp 12

--- models/task_queue5.sh
+++ models/task_queue5.sh
@@ -3,17 +3,17 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-5 --log obs5/pl7e-5 --save weights/exp12/pl7e-5.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 3e-5 --log obs5/pl3e-5 --save weights/exp12/pl3e-5.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-5 --log obs5/pl1e-5 --save weights/exp12/pl1e-5.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-6 --log obs5/pl7e-6 --save weights/exp12/pl7e-6.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 3e-6 --log obs5/pl3e-6 --save weights/exp12/pl3e-6.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-6 --log obs5/pl1e-6 --save weights/exp12/pl1e-6.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-7 --log obs5/pl7e-7 --save weights/exp12/pl7e-7.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 3e-7 --log obs5/pl3e-7 --save weights/exp12/pl3e-7.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-7 --log obs5/pl1e-7 --save weights/exp12/pl1e-7.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-8 --log obs5/pl7e-8 --save weights/exp12/pl7e-8.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 3e-8 --log obs5/pl3e-8 --save weights/exp12/pl3e-8.h5;
-python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-8 --log obs5/pl1e-8 --save weights/exp12/pl1e-8.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-5 --log obs5/pl7e5 --save weights/exp12/pl7e5.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 4e-5 --log obs5/pl4e5 --save weights/exp12/pl4e5.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-5 --log obs5/pl1e5 --save weights/exp12/pl1e5.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-6 --log obs5/pl7e6 --save weights/exp12/pl7e6.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 4e-6 --log obs5/pl4e6 --save weights/exp12/pl4e6.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-6 --log obs5/pl1e6 --save weights/exp12/pl1e6.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-7 --log obs5/pl7e7 --save weights/exp12/pl7e7.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 4e-7 --log obs5/pl4e7 --save weights/exp12/pl4e7.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-7 --log obs5/pl1e7 --save weights/exp12/pl1e7.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 7e-8 --log obs5/pl7e8 --save weights/exp12/pl7e8.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 4e-8 --log obs5/pl4e8 --save weights/exp12/pl4e8.h5;
+python 3DConv.py --epochs 30 --lsdim 16 --pseudos 25 --batch-size 50 --lr 1e-3 --plr 1e-8 --log obs5/pl1e8 --save weights/exp12/pl1e8.h5;
 
 
