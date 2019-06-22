5367510f61193ca3a1741ca3cfaaaaec1b242901
micky774 <zainmeekail@gmail.com>
2019-06-22 00:09:45 -0400

    Fixed task_queue.sh

--- models/task_queue.sh
+++ models/task_queue.sh
@@ -2,5 +2,5 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-task_queue2.sh > weights/exp14/trial.out; 
-task_queue3.sh > weights/exp15/trial.out;
+nohup bash task_queue2.sh > weights/exp14/trial.out; 
+nohup bash task_queue3.sh > weights/exp15/trial.out;
