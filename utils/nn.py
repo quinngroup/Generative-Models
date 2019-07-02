4e0012ac1b17dbac363904a350dba714e442ffbd
AnonymousFool <qsw66793@uga.edu>
2019-07-02 18:46:38 -0400

    Added an asymmetric spatial broadcast decoder to utils.nn

--- utils/nn.py
+++ utils/nn.py
@@ -86,3 +86,41 @@ class true_spatial_broadcast_decoder(nn.Module):
         x = F.leaky_relu(self.conv5(x))
 
         return x
+        
+class spatial_broadcast_decoder_asymmetric(nn.Module):
+    def __init__(self,input_height,input_length,device,lsdim,kernel_size=3,channels=[64,64,64,64]):
+        super(spatial_broadcast_decoder,self).__init__()
+        self.input_height = input_height
+        self.input_length=input_length
+        self.device=device
+        self.lsdim=lsdim
+        assert kernel_size%2==1, "Kernel size must be odd"
+        padding=int((kernel_size-1)/2)
+        #Size-Preserving Convolutions
+        self.conv1 = nn.Conv2d(lsdim + 2, channels[0], kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(channels[0], channels[1], kernel_size=kernel_size, padding=padding)
+        self.conv3 = nn.Conv2d(channels[1], channels[2], kernel_size=kernel_size, padding=padding)
+        self.conv4 = nn.Conv2d(channels[2], channels[3], kernel_size=kernel_size, padding=padding)
+        self.conv5 = nn.Conv2d(channels[3], 1, 1)
+
+    def forward(self,z):
+        baseVector = z.view(-1, self.lsdim, 1, 1)
+        base = baseVector.repeat(1,1,self.input_height,self.input_length)
+
+        heightStepTensor = torch.linspace(-1, 1, self.input_height).to(self.device)
+        lengthStepTensor = torch.linspace(-1, 1, self.input_length).to(self.device)
+        heightAxisVector = heightStepTensor.view(1,1,self.input_height,1)
+        lengthAxisVector = lengthStepTensor.view(1,1,1,self.input_length)
+
+        xPlane = xAxisVector.repeat(z.shape[0],1,1,self.input_length)
+        yPlane = yAxisVector.repeat(z.shape[0],1,self.input_height,1)
+
+        base = torch.cat((xPlane, yPlane, base), 1)         
+
+        x = F.leaky_relu(self.conv1(base))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
+        x = F.leaky_relu(self.conv4(x))
+        x = F.leaky_relu(self.conv5(x))
+
+        return x
