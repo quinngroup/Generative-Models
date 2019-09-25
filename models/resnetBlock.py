d256ba698de9c387640b2cfaec6cc5a56d0155fb
AnonymousFool <qsw66793@uga.edu>
2019-09-25 17:44:13 -0400

    Added error handling

--- models/resnetBlock.py
+++ models/resnetBlock.py
@@ -16,17 +16,19 @@ class ResNetBlock(nn.Module):
     '''
     def __init__(self, channels, kernel_size, numLayers, activationFunction):
         super(ResNetBlock, self).__init__()
+        if type(activationFunction) == tuple and len(activationFunction) != numLayers:
+            raise Exception(f'length of activation function must be same as numLayers {numLayers}, but is instead {len(activationFunction)}')
         self.activationFunction = activationFunction
         if type(kernel_size) == int:
             if kernel_size % 2 == 0:
-                print(f'Error: kernel_size must exclusively have odd values, but has value {kernel_size}')
+                raise Exception(f'kernel_size must exclusively have odd values, but has value {kernel_size}')
                 return
             self.layers = nn.ModuleList([nn.Conv2d(channels, channels, kernel_size, padding=kernel_size//2) for i in range(numLayers)])
 
         else:
             for i in range(2):
                 if kernel_size[i] % 2 == 0:
-                    print(f'Error: kernel_size must exclusively have odd values, but has value {kernel_size[0]}')
+                    raise Exception(f'kernel_size must exclusively have odd values, but has value {kernel_size[0]}')
                     return
             self.layers = nn.ModuleList([nn.Conv2d(channels, channels, kernel_size, padding=(kernel_size[0]//2, kernel_size[1]//2)) for i in range(numLayers)])
     def forward(self, x):
