773f90d90b3c8d035e8cfcae6b21e4ec741ec542
AnonymousFool <qsw66793@uga.edu>
2019-09-26 17:31:11 -0400

    Added a Spatial Broadcast Decoder with Batch Normalization

--- utils/nn.py
+++ utils/nn.py
@@ -50,6 +50,84 @@ class spatial_broadcast_decoder(nn.Module):
         x = F.leaky_relu(self.conv5(x))
 
         return x
+        
+'''
+Spatial Broadcast Decoder with Batch Normalization
+
+@author Quinn Wyner
+'''
+class spatial_broadcast_decoder_batchnorm(nn.Module):
+    def __init__(self,input_length,device,lsdim,kernel_size=3,channels=[64,64,64,64]):
+        super(spatial_broadcast_decoder_batchnorm,self).__init__()
+        self.input_length=input_length
+        self.device=device
+        self.lsdim=lsdim
+        assert kernel_size%2==1, "Kernel size must be odd"
+        padding=int((kernel_size-1)/2)
+        
+        #Size-Preserving Convolutions
+        
+        #(lsdim + 2, input_length, input_length) -> (channels[0], input_length, input_length)
+        self.conv1 = nn.Conv2d(lsdim + 2, channels[0], kernel_size=kernel_size, padding=padding)
+        
+        #(channels[0], input_length, input_length) -> (channels[0], input_length, input_length)
+        self.batch1 = nn.BatchNorm2d(channels[0])
+        
+        #(channels[0], input_length, input_length) -> (channels[1], input_length, input_length)
+        self.conv2 = nn.Conv2d(channels[0], channels[1], kernel_size=kernel_size, padding=padding)
+        
+        #(channels[1], input_length, input_length) -> (channels[1], input_length, input_length)
+        self.batch2 = nn.BatchNorm2d(channels[1])
+        
+        #(channels[1], input_length, input_length) -> (channels[2], input_length, input_length)
+        self.conv3 = nn.Conv2d(channels[1], channels[2], kernel_size=kernel_size, padding=padding)
+        
+        #(channels[2], input_length, input_length) -> (channels[2], input_length, input_length)
+        self.batch3 = nn.BatchNorm2d(channels[2])
+        
+        #(channels[2], input_length, input_length) -> (channels[3], input_length, input_length)
+        self.conv4 = nn.Conv2d(channels[2], channels[3], kernel_size=kernel_size, padding=padding)
+        
+        #(channels[3], input_length, input_length) -> (channels[3], input_length, input_length)
+        self.batch4 = nn.BatchNorm2d(channels[3])
+        
+        #(channels[3], input_length, input_length) -> (1, input_length, input_length)
+        self.conv5 = nn.Conv2d(channels[3], 1, 1)
+        
+        #(1, input_length, input_length) -> (1, input_length, input_length)
+        self.batch5 = nn.BatchNorm2d(1)
+
+    def forward(self,z):
+        baseVector = z.view(-1, self.lsdim, 1, 1)
+        base = baseVector.repeat(1,1,self.input_length,self.input_length)
+
+        stepTensor = torch.linspace(-1, 1, self.input_length).to(self.device)
+
+        xAxisVector = stepTensor.view(1,1,self.input_length,1)
+        yAxisVector = stepTensor.view(1,1,1,self.input_length)
+
+        xPlane = xAxisVector.repeat(z.shape[0],1,1,self.input_length)
+        yPlane = yAxisVector.repeat(z.shape[0],1,self.input_length,1)
+
+        base = torch.cat((xPlane, yPlane, base), 1)         
+
+        #(lsdim+2, input_length, input_length) -> (channels[0], input_length, input_length)
+        x = F.leaky_relu(self.batch1(self.conv1(base)))
+        
+        #(channels[0], input_length, input_length) -> (channels[1], input_length, input_length)
+        x = F.leaky_relu(self.batch2(self.conv2(x)))
+        
+        #(channels[1], input_length, input_length) -> (channels[2], input_length, input_length)
+        x = F.leaky_relu(self.batch3(self.conv3(x)))
+        
+        #(channels[2], input_length, input_length) -> (channels[3], input_length, input_length)
+        x = F.leaky_relu(self.batch4(self.conv4(x)))
+        
+        #(channels[3], input_length, input_length) -> (1, input_length, input_length)
+        x = F.leaky_relu(self.batch5(self.conv5(x)))
+
+        return x
+        
 class true_spatial_broadcast_decoder(nn.Module):
     def __init__(self,input_length,device,lsdim,kernel_size=3,channels=[64,64,64,64]):
         super(true_spatial_broadcast_decoder,self).__init__()
