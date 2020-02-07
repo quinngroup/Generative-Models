f1ee2036c68cd57a45e64b2e6bc1cba5f2fb1b97
Meekail Zain <zainmeekail@gmail.com>
2020-02-07 17:38:41 -0500

    Changed flag names

--- data/clippedDataGen.py
+++ data/clippedDataGen.py
@@ -10,7 +10,7 @@ Treats 0th axis as time, 1st axis as height, 2nd as width
 '''
 
 def clipper(filename, clipLength):
-    data = np.load(args.loadDirectory+filename, mmap_mode='r')
+    data = np.load(args.source+filename, mmap_mode='r')
     
     clipStart = randint(0, data.shape[0] - clipLength)
     
@@ -18,14 +18,14 @@ def clipper(filename, clipLength):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='clippedDataGen')
-    parser.add_argument('--loadDirectory', type=str, default='./', metavar='ld',
+    parser.add_argument('--source', type=str, default='./', metavar='s',
                         help = 'Name of numpy array file to load (default=\'.\')')
     parser.add_argument('--clipLength', type=int, default=40, metavar='cl',
                         help = 'Length of a single clip (default=40)')
-    parser.add_argument('--saveDirectory', type=str, default='clipped/', metavar='sd',
+    parser.add_argument('--dest', type=str, default='clipped/', metavar='d',
                         help = 'Directory in which to save files (default=\'clipped/\')')
     args = parser.parse_args()
     
-    for file in os.listdir(args.loadDirectory):
+    for file in os.listdir(args.source):
         if file.endswith('.npy'):
-            np.save(args.saveDirectory + file, clipper(file, args.clipLength))
+            np.save(args.dest + file, clipper(file, args.clipLength))
